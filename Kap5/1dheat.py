177cd895756af0b0995b1da9bb6edda6bf680d4f
Leif Rune Hellevik <leifh@iktductus.ivt.ntnu.no>
2014-03-12 12:55:46 +0100

    struggeling...

--- Kap5/1dheat.py
+++ Kap5/1dheat.py
@@ -1,5 +1,11 @@
 #-----------------------------------------------------------------------------
-# The equation to be solved 
+# Inspired by 
+# Jonathan Senning Gordon College but...seriously modified by Leif Rune Hellevik march 2014
+#
+# Use FTCS (forward time, centered space) scheme to solve the heat equation
+# in a thin rod.
+#
+# The equation solved is
 #
 #       du     d  du
 #       -- = k -- --
@@ -7,12 +13,12 @@
 #
 # along with boundary conditions
 #
-#       u(xmin,t) = U0
-#       u(xmax,t) = 0
+#       u(xmin,t) = a(t)
+#       u(xmax,t) = b(t)
 #
 # and initial conditions
 #
-#       u(x,tmin) = 0
+#       u(x,tmin) = f(x)
 #
 #-----------------------------------------------------------------------------
 
@@ -21,7 +27,6 @@ import numpy as np
 from pylab import *
 import time as timelib
 
-<<<<<<< HEAD
 
 def f(x):
     return np.sin(np.pi*x) + 3.0*np.sin(2*np.pi*x)
@@ -32,40 +37,28 @@ def f2(x):
 def uexact(x,t):
     return np.sin(np.pi*x)*np.exp(-np.pi**2*t) +3.0*np.sin(2*np.pi*x)*np.exp(-4*np.pi**2*t) 
 
-def uexact2(x,t,U0,L):
-    n=2
-    omegas=np.pi*(2.0*np.linspace(1,n,n)+1)/2/L
-    
-    for omegan in omegas:
-        bn=(alpha*omegan)**2
-        u += np.cos(omegan*x)*np.exp(-bn*t) 
-        
-    return u*U0
 
-=======
->>>>>>> 40178d26f879178ba83bc3d585033aba10c3922a
 k  = 0.25                       #Thermal diffusivity
 (xmin, xmax) = (0,1)
 n  = 20; #32;                    # Number of spatial intervals
 dx = (xmax-xmin)/float(n)
 x = np.linspace(xmin,xmax,n+1)
 
-r=0.5                       #Numerical Fourier number
-dt=r*dx**2/k**2             #Compute timestep based on Fourier number, spatial discretization and thermal diffusivity
+r=0.5                          #Numerical Fourier number
+dt=r*dx**2/k**2                   #Compute timestep based on Fourier number, spatial discretization and thermal diffusivity
 print 'timestep = ',dt
-(tmin, tmax)=(0,2.0)
+(tmin, tmax)=(0,0.5)
 
-
-m=round((tmax-tmin)/dt)                  # Number of temporal intervals
+#m = 128; #4096;                 # Number of temporal intervals
+m=round((tmax-tmin)/dt)
 time=np.linspace(tmin,tmax,m)
-U0 = 4
+
 print 'number of timesteps: ',m
-
 u=np.zeros((n+1,1),float)
-u[0]=U0
+u[0]=1
+u=uexact(x,0.0)
+
 (umin,umax)=(min(u),max(u))
-
-
 #Plot initial solution
 fig = plt.figure()
 ax=fig.add_subplot(111)
@@ -73,11 +66,11 @@ Curve, = ax.plot( x, u[:], '-')
 ax.set_xlim([xmin,xmax])
 ax.set_ylim([umin,umax])
 plt.xlabel('x')
-plt.ylabel('Velocity')
+plt.ylabel('Temperature')
 
 plt.ion()
 plt.show()
-nOutputInt=1
+nOutputInt=10
 i = 0
 
 for t in time:
