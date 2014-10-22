b16dc71f81486b08261cfc09a7421d73734c68c7
Johan Kolstø Sønstabø <johan.k.sonstabo@ntnu.no>
2014-10-22 20:24:41 +0200

    Added a folder called "Utviklingsprosjekt" under allfiles. Doconce script is in the file main.do.txt. Example 1.2 is finished.

---
+++ allfiles/Utviklingsprosjekt/eks1.2/CDsphere.py
@@ -0,0 +1,48 @@
+from numpy import logspace, zeros
+
+def CDsphere(Re):
+    "Computes the drag coefficient of a sphere as a function of the Reynolds number Re."
+    # Curve fitted after fig . A -56 in Evett & Liu :% " Fluid Mechanics & Hydraulics ",
+    # Schaum ' s Solved Problems McGraw - Hill 1989.
+    
+    from numpy import log10, array, polyval
+    
+    if Re <= 0.0:
+        CD = 0.0
+    elif Re > 8.0e6:
+        CD = 0.2
+    elif Re > 0.0 and Re <= 0.5:
+        CD = 24.0/Re
+    elif Re > 0.5 and Re <= 100.0:
+        p = array([4.22, -14.05, 34.87, 0.658])
+        CD = polyval(p, 1.0/Re) 
+    elif Re > 100.0 and Re <= 1.0e4:
+        p = array([-30.41, 43.72, -17.08, 2.41])
+        CD = polyval(p, 1.0/log10(Re))
+    elif Re > 1.0e4 and Re <= 3.35e5:
+        p = array([-0.1584, 2.031, -8.472, 11.932])
+        CD = polyval(p, log10(Re))
+    elif Re > 3.35e5 and Re <= 5.0e5:
+        x1 = log10(Re/4.5e5)
+        CD = 91.08*x1**4 + 0.0764
+    else:
+        p = array([-0.06338, 1.1905, -7.332, 14.93])
+        CD = polyval(p, log10(Re))
+    return CD
+
+# Make plot
+from matplotlib import pyplot
+Re = logspace(-1, 7, 500, True, 10)
+CD = zeros(500)
+for i in range(0,500-1):
+    CD[i] = CDsphere(Re[i])
+font = {'size'   : 16}
+pyplot.rc('font', **font)
+pyplot.plot(Re, CD, '-b')
+pyplot.yscale('log')
+pyplot.xscale('log')
+pyplot.xlabel('$Re$')
+pyplot.ylabel('$C_D$')
+pyplot.grid('on', 'both', 'both')
+#pyplot.show()
+pyplot.savefig('example_sphere.png')
--- allfiles/test_johan/readme.txt
+++
@@ -1,3 +0,0 @@
-To generate a html script, run the command "doconce format html <filename>
---html_style=bootstrap_bluegray"
-
