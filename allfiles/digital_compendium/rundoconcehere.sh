aa497f15a070bf8ddb2b55fed8f18ddc27c0bc8b
Hans Petter Langtangen <hpl@simula.no>
2014-12-29 13:09:01 +0100

    Fixes by hpl.

--- allfiles/digital_compendium/rundoconcehere.sh
+++ allfiles/digital_compendium/rundoconcehere.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+set -x # display shell commands prior to execution
 name=main
 wrap=$name
 
-doconce format html $wrap --html_style=bootstrap_bluegray --examples_as_exercises --html_links_in_new_window --device=screen 
-doconce replace '<hr class="figure">' ' ' *.html
-doconce replace 'https://raw.github.com/hplgit/doconce/master/bundled/html_styles/style_bootstrap/css/bootstrap_bluegray.css' 'bootstrap_bluegray.css' *.html
-sed -i '1i <!DOCTYPE HTML>' *.html
-doconce split_html $wrap.html --pagination
+doconce format html $wrap --html_style=bootstrap_bluegray --examples_as_exercises --html_links_in_new_window --device=screen  --html_figure_hrule=none --html_DOCTYPE --encoding=utf-8
+doconce split_html $wrap.html --pagination --nav_button=top+bottom
+#exit
+
+# Wise to run latex now and then to get error messages in equations
+# (will lead to display errors in MathJax anyway...)
+doconce format pdflatex $wrap --device=paper --encoding=utf-8
+doconce ptex2tex $wrap envir=Verbatim  # very simple code envir
+pdflatex $wrap
+bibtex $wrap
+pdflatex $wrap
+pdflatex $wrap
 
 #--urlcheck
 #--section_numbering=on (problem with example as exercises)
