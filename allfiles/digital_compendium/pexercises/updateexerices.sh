6f170a4b57a5246d7590f55998c6e28a617d8a89
lrhgit <leif.r.hellevik@ntnu.no>
2015-01-08 14:59:07 +0100

    updates

---
+++ allfiles/digital_compendium/pexercises/updateexerices.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SVNDIR=~/Documents/tkt4140/Pythonovingar
+
+#echo $SVNDIR/tkt4140/Pythonovingar/poving?
+
+svndir_length=${#SVNDIR}
+
+WD=$(pwd)
+
+#echo 'you are here:' $WD
+
+
+for oving in $(ls -d $SVNDIR/poving?) ; do
+    local_oving=${oving:svndir_length+1}
+    echo "mkdir" $local_oving
+    mkdir $local_oving
+
+    for files in $(ls $oving/python_exercise_?.html); do
+	echo "cp" $files  $local_oving
+	cp $files $local_oving
+    done
+    for files in $(ls $oving/*.png); do
+	echo "cp" $files $local_oving
+	cp $files $local_oving
+    done
+
+done
+
